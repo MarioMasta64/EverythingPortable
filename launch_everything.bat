3120ed8656b66a6b36fc6dcf47bd68884aa44f36
Isaiah Love <MarioMasta64@users.noreply.github.com>
2017-03-24 09:34:33 -0400

    Add files via upload

---
+++ launch_everything.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo put all your skills into an awesome downloader
+echo profile menus for available programs
+echo download scripts for downloading etc...
+
+echo.
+echo here lies the end
+pause
