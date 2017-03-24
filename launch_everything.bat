c64887366bce2e71c3c83906a8d46ec417209538
Isaiah Love <MarioMasta64@users.noreply.github.com>
2017-03-24 09:36:37 -0400

    Add files via upload

--- launch_everything.bat
+++ launch_everything.bat
@@ -3,6 +3,8 @@ cls
 echo put all your skills into an awesome downloader
 echo profile menus for available programs
 echo download scripts for downloading etc...
+echo add an updater to it to to check the versions :o
+echo updater for this program too?
 
 echo.
 echo here lies the end
