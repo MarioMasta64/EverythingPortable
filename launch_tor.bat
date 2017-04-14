51c0c90f2787be670e462b1e39647c4983253b2b
Isaiah Love <MarioMasta64@users.noreply.github.com>
2017-04-14 06:39:40 -0400

    Add files via upload

---
+++ launch_tor.bat
@@ -0,0 +1,284 @@
+@echo off
+setlocal enabledelayedexpansion
+Color 0A
+cls
+title PORTABLE TOR LAUNCHER
+set nag=BE SURE TO TURN CAPS LOCK OFF! (never said it was on just make sure)
+set new_version=OFFLINE
+if exist replacer.bat del replacer.bat
+
+if not exist .\bin\tor\ mkdir .\bin\tor\
+if not exist .\doc\ mkdir .\doc\
+if not exist .\extra\ mkdir .\extra\
+
+:VERSION
+cls
+echo 1 > .\doc\version.txt
+set /p current_version=<.\doc\version.txt
+if exist .\doc\version.txt del .\doc\version.txt
+
+:CREDITS
+cls
+if exist .\doc\tor_license.txt goto TORCHECK
+echo ================================================== > .\doc\tor_license.txt
+echo =              Script by MarioMasta64            = >> .\doc\tor_license.txt
+:: REMOVE SPACE AFTER VERSION HITS DOUBLE DIGITS
+echo =           Script Version: v%current_version%- release         = >> .\doc\tor_license.txt
+echo ================================================== >> .\doc\tor_license.txt
+echo =You may Modify this WITH consent of the original= >> .\doc\tor_license.txt
+echo = creator, as long as you include a copy of this = >> .\doc\tor_license.txt
+echo =      as you include a copy of the License      = >> .\doc\tor_license.txt
+echo ================================================== >> .\doc\tor_license.txt
+echo =    You may also modify this script without     = >> .\doc\tor_license.txt
+echo =         consent for PERSONAL USE ONLY          = >> .\doc\tor_license.txt
+echo ================================================== >> .\doc\tor_license.txt
+
+:CREDITSREAD
+cls
+title PORTABLE TOR LAUNCHER - ABOUT
+for /f "DELIMS=" %%i in (.\doc\tor_license.txt) do (echo %%i)
+pause
+
+:TORCHECK
+cls
+if not exist .\bin\tor\firefox.exe goto FILECHECK
+goto WGETUPDATE
+
+:FILECHECK
+cls
+if not exist .\extra\torbrowser-install-6.5.1_en-US.exe goto DOWNLOADTOR
+if not exist .\bin\7-ZipPortable\7-ZipPortable.exe goto 7ZIPINSTALLERCHECK
+.\bin\7-ZipPortable\App\7-Zip%arch%\7z.exe x .\extra\torbrowser-install-6.5.1_en-US.exe * -o.\bin\tor\
+rmdir /s /q .\bin\tor\$PLUGINSDIR\
+cd bin\tor\Browser
+xcopy /q .\* .. /e /i /y
+cd ..
+cd ..
+cd ..
+goto TORCHECK
+
+:DOWNLOADTOR
+cls
+if exist torbrowser-install-6.5.1_en-US.exe goto MOVETOR
+if not exist .\bin\wget.exe call :DOWNLOADWGET
+.\bin\wget.exe https://github.com/TheTorProject/gettorbrowser/releases/download/v6.5.1/torbrowser-install-6.5.1_en-US.exe
+
+:MOVETOR
+cls
+move torbrowser-install-6.5.1_en-US.exe .\extra\torbrowser-install-6.5.1_en-US.exe
+goto FILECHECK
+
+:7ZIPINSTALLERCHECK
+if not exist .\extra\7-ZipPortable_16.04.paf.exe goto DOWNLOAD7ZIP
+start .\extra\7-ZipPortable_16.04.paf.exe /destination="%CD%\bin\"
+:: maybe a different approach of bringing this to the front
+cls
+title READMEREADMEREADMEREADMEREADMEREADMEREADMEREADMEREADMEREADMEREADMEREADMEREADMEREADMEREADME
+echo go through the install directions as it says then press enter to continue
+pause
+goto FILECHECK
+
+:DOWNLOAD7ZIP
+cls
+if exist 7-ZipPortable_16.04.paf.exe goto MOVE7ZIP
+if not exist .\bin\wget.exe call :DOWNLOADWGET
+.\bin\wget.exe http://downloads.sourceforge.net/portableapps/7-ZipPortable_16.04.paf.exe
+
+:MOVE7ZIP
+cls
+move 7-ZipPortable_16.04.paf.exe .\extra\7-ZipPortable_16.04.paf.exe
+goto FILECHECK
+
+:WGETUPDATE
+cls
+wget https://eternallybored.org/misc/wget/current/wget.exe
+move wget.exe .\bin\
+goto MENU
+
+:DOWNLOADWGET
+cls
+call :CHECKWGETDOWNLOADER
+exit /b
+
+:CHECKWGETDOWNLOADER
+cls
+if not exist .\bin\downloadwget.vbs call :CREATEWGETDOWNLOADER
+if exist .\bin\downloadwget.vbs call :EXECUTEWGETDOWNLOADER
+exit /b
+
+:CREATEWGETDOWNLOADER
+cls
+echo ' Set your settings > .\bin\downloadwget.vbs
+echo    strFileURL = "https://eternallybored.org/misc/wget/current/wget.exe" >> .\bin\downloadwget.vbs
+echo    strHDLocation = "wget.exe" >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo ' Fetch the file >> .\bin\downloadwget.vbs
+echo     Set objXMLHTTP = CreateObject("MSXML2.XMLHTTP") >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo     objXMLHTTP.open "GET", strFileURL, false >> .\bin\downloadwget.vbs
+echo     objXMLHTTP.send() >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo If objXMLHTTP.Status = 200 Then >> .\bin\downloadwget.vbs
+echo Set objADOStream = CreateObject("ADODB.Stream") >> .\bin\downloadwget.vbs
+echo objADOStream.Open >> .\bin\downloadwget.vbs
+echo objADOStream.Type = 1 'adTypeBinary >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objADOStream.Write objXMLHTTP.ResponseBody >> .\bin\downloadwget.vbs
+echo objADOStream.Position = 0    'Set the stream position to the start >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo Set objFSO = Createobject("Scripting.FileSystemObject") >> .\bin\downloadwget.vbs
+echo If objFSO.Fileexists(strHDLocation) Then objFSO.DeleteFile strHDLocation >> .\bin\downloadwget.vbs
+echo Set objFSO = Nothing >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objADOStream.SaveToFile strHDLocation >> .\bin\downloadwget.vbs
+echo objADOStream.Close >> .\bin\downloadwget.vbs
+echo Set objADOStream = Nothing >> .\bin\downloadwget.vbs
+echo End if >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo Set objXMLHTTP = Nothing >> .\bin\downloadwget.vbs
+exit /b
+
+:EXECUTEWGETDOWNLOADER
+cls
+title PORTABLE TOR LAUNCHER - DOWNLOAD WGET
+cscript.exe .\bin\downloadwget.vbs
+move wget.exe .\bin\
+exit /b
+
+:MENU
+cls
+title PORTABLE TOR LAUNCHER - MAIN MENU
+echo %NAG%
+set nag=SELECTION TIME!
+echo 1. reinstall tor [not a feature yet]
+echo 2. launch tor
+echo 3. reset tor [not a feature yet]
+echo 4. uninstall tor [not a feature yet]
+echo 5. update program
+echo 6. about
+echo 7. exit
+echo.
+echo b. download other projects
+echo.
+set /p choice="enter a number and press enter to confirm: "
+if "%choice%"=="1" goto NEW
+if "%choice%"=="2" goto DEFAULT
+if "%choice%"=="3" goto SELECT
+if "%choice%"=="4" goto DELETE
+if "%choice%"=="5" goto UPDATECHECK
+if "%choice%"=="6" goto ABOUT
+if "%choice%"=="7" goto EXIT
+if "%CHOICE%"=="b" goto PORTABLEEVERYTHING
+set nag="PLEASE SELECT A CHOICE 1-7"
+goto MENU
+
+:NULL
+cls
+set nag="NOT A FEATURE YET!"
+goto MENU
+
+:NEW
+cls
+goto NULL
+
+:DEFAULT
+cls
+start .\bin\tor\firefox.exe
+exit
+
+:NOWRESETTING
+goto NULL
+
+:DELETE
+goto NULL
+
+:UPDATECHECK
+cls
+if exist version.txt del version.txt
+if not exist .\bin\wget.exe call :DOWNLOADWGET
+.\bin\wget.exe https://raw.githubusercontent.com/MarioMasta64/EverythingPortable/master/version.txt
+set Counter=0 & for /f "DELIMS=" %%i in ('type version.txt') do (set /a Counter+=1 & set "Line_!Counter!=%%i")
+if exist version.txt del version.txt
+set new_version=%Line_18%
+if %new_version%==OFFLINE goto ERROROFFLINE
+if %current_version% EQU %new_version% goto LATEST
+if %current_version% LSS %new_version% goto NEWUPDATE
+if %current_version% GTR %new_version% goto NEWEST
+goto ERROROFFLINE
+
+:LATEST
+cls
+title PORTABLE TOR LAUNCHER - LATEST BUILD :D
+echo you are using the latest version!!
+echo Current Version: v%current_version%
+echo New Version: v%new_version%
+echo ENTER TO CONTINUE
+pause
+goto MENU
+
+:NEWUPDATE
+cls
+title PORTABLE TOR LAUNCHER - OLD BUILD D:
+echo %NAG%
+set nag=SELECTION TIME!
+echo you are using an older version
+echo enter yes or no
+echo Current Version: v%current_version%
+echo New Version: v%new_version%
+set /p choice="Update?: "
+if "%choice%"=="yes" goto UPDATE
+if "%choice%"=="no" goto MENU
+set nag="please enter YES or NO"
+goto NEWUPDATE
+
+:UPDATE
+cls
+if not exist .\bin\wget.exe call :DOWNLOADWGET
+.\bin\wget.exe https://raw.githubusercontent.com/MarioMasta64/EverythingPortable/master/launch_tor.bat
+if exist launch_tor.bat.1 goto REPLACERCREATE
+goto ERROROFFLINE
+
+:REPLACERCREATE
+cls
+echo del launch_tor.bat >> replacer.bat
+echo rename launch_tor.bat.1 launch_tor.bat >> replacer.bat
+echo start launch_tor.bat >> replacer.bat
+echo exit >> replacer.bat
+start replacer.bat
+exit
+
+:NEWEST
+cls
+title PORTABLE TOR LAUNCHER - TEST BUILD :0
+echo YOURE USING A TEST BUILD MEANING YOURE EITHER
+echo CLOSE TO ME OR YOURE SOME SORT OF PIRATE
+echo Current Version: v%current_version%
+echo New Version: v%new_version%
+echo ENTER TO CONTINUE
+pause
+start launch_tor.bat
+exit
+
+:ABOUT
+cls
+del .\doc\tor_license.txt
+start launch_tor.bat
+exit
+
+:PORTABLEEVERYTHING
+cls
+if not exist .\bin\wget.exe call :DOWNLOADWGET
+if not exist launch_everything.bat .\bin\wget.exe https://raw.githubusercontent.com/MarioMasta64/EverythingPortable/master/launch_everything.bat
+start launch_everything.bat
+exit
+
+:ERROROFFLINE
+cls
+set nag="YOU SEEM TO BE OFFLINE PLEASE RECONNECT TO THE INTERNET TO USE THIS FEATURE"
+goto MENU
+
+:ERROR
+cls
+echo ERROR OCCURED
+pause
+goto MENU
