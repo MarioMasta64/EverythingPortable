fe00611974338f63247cc78b2ff3718f9a8853fd
Isaiah Love <MarioMasta64@users.noreply.github.com>
2017-06-05 22:05:48 -0400

    Double poc shall i merge?

---
+++ launch_teamviewer_host_poc.bat
@@ -0,0 +1,74 @@
+@echo off
+cls
+Color 0A
+
+if not exist .\bin\teamviewer_host\ mkdir .\bin\teamviewer_host\
+if not exist .\data\AppData\Roaming\ mkdir .\data\AppData\Roaming\
+if not exist .\extra\ mkdir .\extra\
+
+echo "l" to launch teamviewer host
+echo "d" to download teamviewer host
+set /p goto="choice: "
+goto %goto%
+
+:d
+cls
+echo ' Set your settings > .\bin\downloadwget.vbs
+echo strFileURL = "https://eternallybored.org/misc/wget/current/wget.exe" >> .\bin\downloadwget.vbs
+echo strHDLocation = "wget.exe" >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo ' Fetch the file >> .\bin\downloadwget.vbs
+echo Set objXMLHTTP = CreateObject("MSXML2.XMLHTTP") >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objXMLHTTP.open "GET", strFileURL, false >> .\bin\downloadwget.vbs
+echo objXMLHTTP.send() >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo If objXMLHTTP.Status = 200 Then >> .\bin\downloadwget.vbs
+echo Set objADOStream = CreateObject("ADODB.Stream") >> .\bin\downloadwget.vbs
+echo objADOStream.Open >> .\bin\downloadwget.vbs
+echo objADOStream.Type = 1 'adTypeBinary >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objADOStream.Write objXMLHTTP.ResponseBody >> .\bin\downloadwget.vbs
+echo objADOStream.Position = 0'Set the stream position to the start >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo Set objFSO = Createobject("Scripting.FileSystemObject") >> .\bin\downloadwget.vbs
+echo If objFSO.Fileexists(strHDLocation) Then objFSO.DeleteFile strHDLocation >> .\bin\downloadwget.vbs
+echo Set objFSO = Nothing >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objADOStream.SaveToFile strHDLocation >> .\bin\downloadwget.vbs
+echo objADOStream.Close >> .\bin\downloadwget.vbs
+echo Set objADOStream = Nothing >> .\bin\downloadwget.vbs
+echo End if >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo Set objXMLHTTP = Nothing >> .\bin\downloadwget.vbs
+cscript.exe .\bin\downloadwget.vbs
+move wget.exe .\bin\wget.exe
+.\bin\wget.exe http://downloads.sourceforge.net/portableapps/7-ZipPortable_16.04.paf.exe
+.\bin\wget.exe https://download.teamviewer.com/download/TeamViewer_Host_Setup.exe
+move 7-ZipPortable_16.04.paf.exe .\extra\7-ZipPortable_16.04.paf.exe
+move TeamViewer_Host_Setup.exe .\extra\TeamViewer_Host_Setup.exe
+start .\extra\7-ZipPortable_16.04.paf.exe /destination="%CD%\bin\"
+if not exist .\temp\ mkdir .\temp\
+:: is not required to be set. will be set in release
+.\bin\7-ZipPortable\App\7-Zip%arch%\7z.exe x .\extra\TeamViewer_Host_Setup.exe * -o.\temp\
+rmdir /s /q .\temp\$PLUGINSDIR\
+del .\temp\tvfilesx64.7z
+del .\temp\tvfilesx86.7z
+:: is not required to be set. will be set in release
+.\bin\7-ZipPortable\App\7-Zip%arch%\7z.exe x .\temp\tvfiles.7z * -o.\bin\teamviewer_host\
+rmdir /s /q .\temp\
+del .\bin\teamviewer_host\Uninstall.exe
+
+:l
+cls
+echo when the popup appears say no.
+echo when the firewall pops up say cancel.
+echo Failed to update TeamViewer service properties.
+echo this is normal click ok.
+echo the program will now start
+echo teamviewer will not be started with windows btw.
+echo ready to connect
+pause
+set "AppData=%CD%\data\AppData\Roaming"
+start .\bin\teamviewer_host\TeamViewer.exe
+exit
