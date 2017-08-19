58310a674770305cc837286e7372cc0a4cb3ee70
Isaiah Love <loveme0198@gmail.com>
2017-08-18 20:40:36 -0400

    if exist .\doc\obs_license.txt (goto) 2>nul

--- launch_obs.bat
+++ launch_obs.bat
@@ -265,6 +265,7 @@ if exist .\doc\version.txt del .\doc\version.txt
 
 :Credits
 cls
+if exist .\doc\obs_license.txt (goto) 2>nul
 echo ================================================== > .\doc\obs_license.txt
 echo =              Script by MarioMasta64            = >> .\doc\obs_license.txt
 echo =           Script Version: v%current_version%- release        = >> .\doc\obs_license.txt
