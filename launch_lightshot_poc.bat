5ef7580a92857d7eda21936ac6757fe645de4c8d
Isaiah Love <loveme0198@gmail.com>
2017-08-24 18:04:24 -0400

    add lightshot

---
+++ launch_lightshot_poc.bat
@@ -0,0 +1,70 @@
+@echo off
+cls
+Color 0A
+
+if not exist .\bin\ mkdir .\bin\
+if not exist .\data\AppData\Roaming\ mkdir .\data\AppData\Roaming\
+if not exist .\extra\ mkdir .\extra\
+
+echo "l" to launch lightshot 
+echo "d" to download lightshot (first time)
+echo "e" to extract lightshot 
+echo "u" to update lightshot 
+set /p goto="choice: "
+goto %goto%
+
+:d
+cls
+echo ' Set your settings > .\bin\downloadwget.vbs
+echo strFileURL = "https://eternallybored.org/misc/wget/current/wget.exe" >> .\bin\downloadwget.vbs
+echo strHDLocation = "wget.exe" >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo ' Fetch the file >> .\bin\downloadwget.vbs
+echo Set objXMLHTTP = CreateObject("MSXML2.XMLHTTP") >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objXMLHTTP.open "GET", strFileURL, false >> .\bin\downloadwget.vbs
+echo objXMLHTTP.send() >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo If objXMLHTTP.Status = 200 Then >> .\bin\downloadwget.vbs
+echo Set objADOStream = CreateObject("ADODB.Stream") >> .\bin\downloadwget.vbs
+echo objADOStream.Open >> .\bin\downloadwget.vbs
+echo objADOStream.Type = 1 'adTypeBinary >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objADOStream.Write objXMLHTTP.ResponseBody >> .\bin\downloadwget.vbs
+echo objADOStream.Position = 0'Set the stream position to the start >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo Set objFSO = Createobject("Scripting.FileSystemObject") >> .\bin\downloadwget.vbs
+echo If objFSO.Fileexists(strHDLocation) Then objFSO.DeleteFile strHDLocation >> .\bin\downloadwget.vbs
+echo Set objFSO = Nothing >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo objADOStream.SaveToFile strHDLocation >> .\bin\downloadwget.vbs
+echo objADOStream.Close >> .\bin\downloadwget.vbs
+echo Set objADOStream = Nothing >> .\bin\downloadwget.vbs
+echo End if >> .\bin\downloadwget.vbs
+echo. >> .\bin\downloadwget.vbs
+echo Set objXMLHTTP = Nothing >> .\bin\downloadwget.vbs
+cscript.exe .\bin\downloadwget.vbs
+move wget.exe .\bin\wget.exe
+.\bin\wget.exe http://downloads.sourceforge.net/portableapps/7-ZipPortable_16.04.paf.exe
+move 7-ZipPortable_16.04.paf.exe .\extra\7-ZipPortable_16.04.paf.exe
+.\extra\7-ZipPortable_16.04.paf.exe /destination="%CD%\bin\"
+
+:u
+.\bin\wget.exe https://sourceforge.net/projects/innounp/files/latest/download?source=typ_redirect
+rename "download@source=typ_redirect" innounp.rar
+move innounp.rar .\extra\innounp.rar
+:: is not required to be set. will be set in release
+.\bin\7-ZipPortable\App\7-Zip%arch%\7z.exe x .\extra\innounp.rar * -o.\bin\innounp\
+.\bin\wget.exe https://app.prntscr.com/build/setup-lightshot.exe
+move setup-lightshot.exe .\extra\setup-lightshot.exe
+
+:e
+.\bin\innounp\innounp.exe -q -x -y -dtemp .\extra\setup-lightshot.exe
+rmdir /s /q .\bin\lightshot\
+xcopy .\temp\{app}\* .\bin\lightshot\ /e /i /y
+rmdir /s /q .\temp\
+
+:l
+set "appdata=%CD%\data\appdata\roaming\"
+start .\bin\lightshot\Lightshot.exe
+exit
