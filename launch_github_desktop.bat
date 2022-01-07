c9537c6f0123bbbd7b1599261a552e6dc7ec2c11
MarioMasta64 <loveme0198@gmail.com>
2022-01-07 05:31:37 -0500

    version update

--- launch_github_desktop.bat
+++ launch_github_desktop.bat
@@ -301,7 +301,7 @@ set "NoPrompt=" & for /F "skip=5 delims=" %%l in (.\ini\settings.ini) do ( set "
 exit /b 2
 
 :Version
-echo 23 > .\doc\version.txt
+echo 24 > .\doc\version.txt
 set /p current_version=<.\doc\version.txt
 if exist .\doc\version.txt del .\doc\version.txt >nul
 exit /b 2
