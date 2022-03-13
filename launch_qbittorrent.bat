c4ad0664e74ff0388d7532509ea0c6a797a49ecd
Isaiah Love <loveme0198@gmail.com>
2022-03-12 22:52:21 -0500

    actually push the fix

--- launch_qbittorrent.bat
+++ launch_qbittorrent.bat
@@ -185,6 +185,7 @@ for /f tokens^=2delims^=^" %%A in (
 if exist download.php del download.php >nul
 set /p qbittorrent_link=<.\doc\qbittorrent_link.txt
 REM no 64bit yet, lazy
+set "qbittorrent_link=!qbittorrent_link:_qt6=!"
 set "qbittorrent_link=!qbittorrent_link:_x64=!"
 set "qbittorrent_link=!qbittorrent_link:/download=!"
 set "tempstr=!qbittorrent_link!"
@@ -322,7 +323,7 @@ set "NoPrompt=" & for /F "skip=5 delims=" %%l in (.\ini\settings.ini) do ( set "
 exit /b 2
 
 :Version
-echo 19 > .\doc\version.txt
+echo 20 > .\doc\version.txt
 set /p current_version=<.\doc\version.txt
 if exist .\doc\version.txt del .\doc\version.txt >nul
 exit /b 2
